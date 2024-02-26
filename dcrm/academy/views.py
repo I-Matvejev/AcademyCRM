37b5a135b4c0ea8e49cf43505d3e17ad37ad80ff
IlyaMatvejev <ilyamatvejev@gmail.com>
2024-02-26 16:00:50 +0300

    Login and alerts working

--- dcrm/academy/views.py
+++ dcrm/academy/views.py
@@ -4,11 +4,17 @@ from django.contrib import messages
 
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Вы успешно авторизовались!")
+            return redirect('home')
+        else:
+            messages.success(request, "Возникла проблема, попробуйте еще раз...")
     return render(request, 'home.html', {})
-
-
-def login_user(request):
-    pass
 
 
 def logout_user(request):
