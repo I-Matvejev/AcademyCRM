ea593a0a048f6c2f57825f60e39959241f30e36b
IlyaMatvejev <ilyamatvejev@gmail.com>
2024-03-26 11:46:05 +0300

    DB restarted with new fields

--- dcrm/academy/forms.py
+++ dcrm/academy/forms.py
@@ -3,14 +3,14 @@ from .models import Course, Attendee
 
 
 class NewCourseForm(forms.ModelForm):
-    course_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Название курса", "class": "form-control"}), label="")
-    course_date_begin = forms.DateField(required=True, widget=forms.widgets.DateInput(format='%d.%m.%Y', attrs={"placeholder": "Дата начала", "class": "form-control"}), input_formats=['%d.%m.%Y'], label="")
-    course_time_begin = forms.TimeField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "Время начала", "class": "form-control"}), label="")
-    course_location = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Место проведения", "class": "form-control"}), label="")
+    course_name = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Название курса", "class": "form-control"}), label="")
+    course_date_begin = forms.DateField(widget=forms.widgets.DateInput(format='%d.%m.%Y', attrs={"placeholder": "Дата начала", "class": "form-control"}), input_formats=['%d.%m.%Y'], label="")
+    course_time_begin = forms.TimeField(initial='10:00', widget=forms.widgets.TimeInput(attrs={"placeholder": "Время начала", "class": "form-control"}), label="Время начала")
+    course_location = forms.CharField(initial='127083, г. Москва, ул. Верхняя Масловка д. 20, стр. 2', widget=forms.widgets.TextInput(attrs={"placeholder": "Место проведения", "class": "form-control"}), label="Адрес")
     course_tutor = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Преподаватель", "class": "form-control"}), label="")
     course_date_end = forms.DateField(widget=forms.widgets.DateInput(format='%d.%m.%Y', attrs={"placeholder": "Дата окончания", "class": "form-control"}), input_formats=['%d.%m.%Y'], label="")
-    course_time_end = forms.TimeField(widget=forms.widgets.TimeInput(attrs={"placeholder": "Время окончания", "class": "form-control"}), label="")
-    course_standard = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Стандарт", "class": "form-control"}), label="")
+    course_time_end = forms.TimeField(initial='17:00', widget=forms.widgets.TimeInput(attrs={"placeholder": "Время окончания", "class": "form-control"}), label="Время окончания")
+    course_status = forms.Select(attrs={"placeholder": "Статус", "class": "form-control"})
 
     class Meta:
         model = Course
@@ -18,19 +18,26 @@ class NewCourseForm(forms.ModelForm):
 
 
 class CourseAttendeesForm(forms.ModelForm):
-    attendee_course_id = forms.ModelChoiceField(queryset=Course.objects.all(), required=True, widget=forms.widgets.HiddenInput(attrs={"placeholder": "Название курса", "class": "form-control"}), label="")
-    attendee_last_name_rus = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Фамилия", "class": "form-control"}), label="")
-    attendee_first_name_rus = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Имя", "class": "form-control"}), label="")
+    attendee_course_id = forms.ModelChoiceField(queryset=Course.objects.all(), widget=forms.widgets.HiddenInput(attrs={"placeholder": "Название курса", "class": "form-control"}), label="")
+    attendee_last_name_rus = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Фамилия", "class": "form-control"}), label="")
+    attendee_first_name_rus = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Имя", "class": "form-control"}), label="")
     attendee_fathers_name_rus = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Отчество", "class": "form-control"}), label="")
-    attendee_last_name_eng = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Surname", "class": "form-control"}), label="")
-    attendee_first_name_eng = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Name", "class": "form-control"}), label="")
-    attendee_fathers_name_eng = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Fathers name", "class": "form-control"}), label="")
-    attendee_gender = forms.Select(attrs={"placeholder": "Пол", "class": "form-control"})
-    attendee_email = forms.EmailField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Электронная почта", "class": "form-control"}), label="")
-    attendee_phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Телефон: +7-999-123-4567", "class": "form-control"}), label="")
-    attendee_company = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Компания", "class": "form-control"}), label="")
-    attendee_position = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Должность", "class": "form-control"}), label="")
-    attendee_city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Город", "class": "form-control"}), label="")
+    attendee_last_name_eng = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Surname", "class": "form-control"}), label="")
+    attendee_first_name_eng = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Name", "class": "form-control"}), label="")
+    attendee_email = forms.EmailField(widget=forms.widgets.TextInput(attrs={"placeholder": "Электронная почта", "class": "form-control"}), label="")
+    attendee_phone = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Телефон", "class": "form-control"}), label="")
+    attendee_company = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Компания", "class": "form-control"}), label="")
+    attendee_position = forms.CharField(widget=forms.widgets.TextInput(attrs={"placeholder": "Должность", "class": "form-control"}), label="")
+    attendee_contract_number = forms.CharField(max_length=25, widget=forms.widgets.TextInput(attrs={"placeholder": "Номер договора", "class": "form-control"}), label="")
+    attendee_contract_status = forms.Select(attrs={"placeholder": "Статус договора", "class": "form-control"})
+    attendee_invoice_status = forms.Select(attrs={"placeholder": "Статус счета", "class": "form-control"})
+    attendee_course_price = forms.CharField(max_length=25, widget=forms.widgets.TextInput(attrs={"placeholder": "Стоимость", "class": "form-control"}), label="")
+    attendee_contact_last_name_rus = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder": "Фамилия контактного лица (опционально)", "class": "form-control"}), label="")
+    attendee_contact_first_name_rus = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder": "Имя контактного лица (опционально)", "class": "form-control"}), label="")
+    attendee_contact_fathers_name_rus = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder": "Отчество контактного лица (опционально)", "class": "form-control"}), label="")
+    attendee_contact_email = forms.EmailField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder": "Электронная почта контактного лица (опционально)", "class": "form-control"}), label="")
+    attendee_contact_phone = forms.CharField(required=False,  widget=forms.widgets.TextInput(attrs={"placeholder": "Телефон контактного лица (опционально)", "class": "form-control"}), label="")
+    attendee_contact_position = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder": "Должность контактного лица (опционально)", "class": "form-control"}), label="")
 
     class Meta:
         model = Attendee
--- dcrm/academy/models.py
+++ dcrm/academy/models.py
@@ -9,15 +9,14 @@ class Course(models.Model):
 
     COURSE_STATUS = [('Запланирован', 'Запланирован'), ('Подтвержден', 'Подтвержден'), ('Проведен', 'Проведен'), ('Отменен', 'Отменен')]
 
-    course_name = models.CharField(max_length=150, blank=False)
-    course_date_begin = models.DateField(blank=False)
-    course_time_begin = models.TimeField(blank=False)
-    course_location = models.CharField(max_length=100)
+    course_name = models.CharField(max_length=150)
+    course_date_begin = models.DateField()
+    course_time_begin = models.TimeField(default='10:00')
+    course_location = models.CharField(max_length=100, default='127083, г. Москва, ул. Верхняя Масловка д. 20, стр. 2')
     course_tutor = models.CharField(max_length=100)
     course_date_end = models.DateField()
-    course_time_end = models.TimeField()
-    course_standard = models.CharField(max_length=100)
-    course_status = models.CharField(blank=False, choices=COURSE_STATUS, default='planned', verbose_name=('Статус'))
+    course_time_end = models.TimeField(default='17:00')
+    course_status = models.CharField(choices=COURSE_STATUS, default='Запланирован', verbose_name=('Статус'))
 
     def __str__(self):
         return f"{self.course_name} {self.course_date_begin}"
@@ -25,21 +24,29 @@ class Course(models.Model):
 
 class Attendee(models.Model):
 
-    GENDER_CHOICES = [('M', 'M'), ('F', 'F')]
+    CONTRACT_CHOICES = [('Не направлен', 'Не направлен'), ('Направлен', 'Направлен'), ('Подписан', 'Подписан')]
+    INVOICE_CHOICES = [('Не оплачен', 'Не оплачен'), ('Оплачен', 'Оплачен'), ('Постоплата', 'Постоплата')]
 
-    attendee_course_id = models.ForeignKey(Course, on_delete=models.CASCADE, blank=False)
-    attendee_last_name_rus = models.CharField(max_length=50, blank=False, validators=[name_validator_rus], default='')
-    attendee_first_name_rus = models.CharField(max_length=20, blank=False, validators=[name_validator_rus], default='')
-    attendee_fathers_name_rus = models.CharField(max_length=50, validators=[name_validator_rus], default='')
-    attendee_last_name_eng = models.CharField(max_length=50, blank=False, validators=[name_validator_eng], default='')
-    attendee_first_name_eng = models.CharField(max_length=20, blank=False, validators=[name_validator_eng], default='')
-    attendee_fathers_name_eng = models.CharField(max_length=50, validators=[name_validator_eng], default='')
-    attendee_gender = models.CharField(blank=False, choices=GENDER_CHOICES, default='M', verbose_name=('Пол'))
-    attendee_email = models.EmailField(max_length=100, blank=False)
-    attendee_phone = models.CharField(max_length=20, blank=False)
+    attendee_course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    attendee_last_name_rus = models.CharField(max_length=50, validators=[name_validator_rus])
+    attendee_first_name_rus = models.CharField(max_length=20, validators=[name_validator_rus])
+    attendee_fathers_name_rus = models.CharField(max_length=50, validators=[name_validator_rus])
+    attendee_last_name_eng = models.CharField(max_length=50, validators=[name_validator_eng])
+    attendee_first_name_eng = models.CharField(max_length=20, validators=[name_validator_eng])
+    attendee_email = models.EmailField(max_length=100)
+    attendee_phone = models.CharField(max_length=20)
     attendee_company = models.CharField(max_length=100)
-    attendee_position = models.CharField(max_length=50, default='')
-    attendee_city = models.CharField(max_length=50)
+    attendee_position = models.CharField(max_length=50)
+    attendee_contract_number = models.CharField(max_length=25)
+    attendee_contract_status = models.CharField(choices=CONTRACT_CHOICES, default='Не направлен', verbose_name=('Статус договора'))
+    attendee_invoice_status = models.CharField(choices=INVOICE_CHOICES, default='Не оплачен', verbose_name=('Статус счета'))
+    attendee_course_price = models.CharField(max_length=25)
+    attendee_contact_last_name_rus = models.CharField(blank=True, max_length=50, validators=[name_validator_rus])
+    attendee_contact_first_name_rus = models.CharField(blank=True, max_length=20, validators=[name_validator_rus])
+    attendee_contact_fathers_name_rus = models.CharField(blank=True, max_length=50, validators=[name_validator_rus])
+    attendee_contact_email = models.EmailField(blank=True, max_length=100)
+    attendee_contact_phone = models.CharField(blank=True, max_length=20)
+    attendee_contact_position = models.CharField(blank=True, max_length=50)
 
     def __str__(self):
         return f"{self.attendee_first_name_rus} {self.attendee_last_name_rus}"
